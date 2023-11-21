811280f72decda991c2438a8b2ce713791c7d5d2
UX7HQ <151670353+UX7HQ@users.noreply.github.com>
2023-11-22 02:45:04 +0800

    Add files via upload

--- scratchGame.js
+++ scratchGame.js
@@ -2,13 +2,25 @@ document.addEventListener('DOMContentLoaded', function () {
     const scratchCard = document.getElementById('scratchCard');
     const scratchLayer = document.getElementById('scratchLayer');
     const ctx = scratchLayer.getContext('2d');
+
+    // 刮前的底图
+    const backgroundImage = new Image();
+    backgroundImage.src = 'cta-2.png'; // 替换为刮前的完整图案路径
+
+    backgroundImage.onload = function() {
+        ctx.drawImage(backgroundImage, 0, 0, scratchLayer.width, scratchLayer.height);
+    };
+
+    // 刮后显示的图案
+    const congratulationsImage = new Image();
+    congratulationsImage.src = 'UX7-logo-BIG-SIZE.png'; // 替换为刮后显示的 "恭喜中奖" 图案路径
+
+    // 记录用户刮的次数
+    let scratchCount = 0;
+
+    // 添加刮开效果
     let isDrawing = false;
 
-    // 初始化刮刮乐图层
-    ctx.fillStyle = '#808080'; // 设置底色，可以是刮刮乐图层的默认颜色
-    ctx.fillRect(0, 0, scratchLayer.width, scratchLayer.height);
-
-    // 监听鼠标或触摸事件
     scratchLayer.addEventListener('mousedown', startDrawing);
     scratchLayer.addEventListener('touchstart', startDrawing);
 
@@ -33,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const offsetX = x - rect.left;
         const offsetY = y - rect.top;
 
-        ctx.globalCompositeOperation = 'destination-out'; // 设置为 destination-out 实现刮开效果
+        ctx.globalCompositeOperation = 'destination-out';
         ctx.beginPath();
         ctx.arc(offsetX, offsetY, 20, 0, 2 * Math.PI);
         ctx.fill();
@@ -41,5 +53,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function stopDrawing() {
         isDrawing = false;
+        scratchCount++;
+
+        if (scratchCount === 1) {
+            // 第一次刮开，显示 "很遗憾，再试一次"
+            alert('很遗憾，再试一次');
+        } else if (scratchCount === 2) {
+            // 第二次刮开，显示 "恭喜中奖"
+            ctx.clearRect(0, 0, scratchLayer.width, scratchLayer.height);
+            ctx.drawImage(congratulationsImage, 0, 0, scratchLayer.width, scratchLayer.height);
+            alert('恭喜中奖');
+        }
     }
 });
